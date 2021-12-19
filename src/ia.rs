0fc541634fd6a305e50233452ad87ae398e8ec72
hender <guiguiruff@gmail.com>
2021-12-19 17:57:35 +0100

    Added base structure for the Ia crate

--- src/ia.rs
+++ src/ia.rs
@@ -2,13 +2,30 @@ use pyo3::prelude::*;
 use pyo3::types::PyTuple;
 
 
-pub struct Ia{}
-
-fn calcul_best_place(u8 raw, u8 rotate){
-    /*score=c1*w1+c2*w2...*/
+pub struct Ia{
+    mat :[[u8; 144]; 160],
+    piece : PieceType,
+    child : [u8; 4]
+    //Jeux de coup ?
+    //
 }
 
-fn find_best_place(/* tetromino et sol*/){
+pub enum PieceType{
+    I,
+    J,
+    L,
+    O,
+    S,
+    Z,
+    T,
+}
+
+
+//fn calcul_best_place(u8 raw, u8 rotate){
+    /*score=c1*w1+c2*w2...*/
+//}
+
+/*fn find_best_place(/* tetromino et sol*/){
     let score = 0;
     let best_score = 0;
     let pose = [0,0];
@@ -24,8 +41,8 @@ fn find_best_place(/* tetromino et sol*/){
     }
     /*call action*/
 }
-
-fn genetic(){
+*/
+//fn genetic(){
     /*
     Generate the initial population
     Compute fitness
