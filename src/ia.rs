1fde0fca7b830275d4e3e67bca8f13f71a82ea9e
hender <guiguiruff@gmail.com>
2021-12-20 00:44:04 +0100

    Fixed some issue in IA and added some arguments to the function declarations

--- src/ia.rs
+++ src/ia.rs
@@ -1,9 +1,6 @@
-use pyo3::prelude::*;
-use pyo3::types::PyTuple;
-
 
 pub struct Ia{
-    mat :[[u8; 144]; 160],
+    mat :[[bool; 144]; 160],
     piece : PieceType,
     child : [u8; 4]
     //Jeux de coup ?
@@ -21,31 +18,32 @@ pub enum PieceType{
 }
 
 /*This function will take a position and then compute a score. Lower is the score better is the position.*/
-fn compute_best_place(/*matrix,w1,w2,w3,w4*/){
+//TODO Make a enum for rotate ?
+fn compute_best_place(matrix : &[[bool; 144]; 160],col : u8, rotate : u8, ) -> u8{
     /*gaps = number of gap
     height_mean = mean of the heigths
     max_diff diff between highest and lowest
     max_side_diff = max dif between two side raw*/
-    let gaps = 0, height_mean = 0, min_height = 15, max_height = 0,max_side_diff;
-    let col_heigths =[0,0,0,0,0,0,0,0,0,0];
-    for collumn in range 0..9{
-        for raw in range 0..15{
-            if matrix[raw][collumn]==0 && col_heigths[collumn]!=0{
+    let mut gaps = 0; height_mean = 0; min_height = 15; max_height = 0; max_side_diff;
+    let mut col_heigth =[0,0,0,0,0,0,0,0,0,0];
+    for column in 0..9{
+        for raw in ra0..15{
+            if matrix[raw][column]== false && col_heigth[column]!=0{
                 gaps+=1;
-            }else if matrix[raw][collumn]==1 && col_heigths[collumn]==0{
-                col_heigths[collumn]=(15-raw);
+            }else if matrix[raw][column]== true && col_heigth[column]==0{
+                col_heigth[column]=(15-raw);
                 height_mean+=(15-raw);
             }
         }
-        if min_height > col_heigths[collumn]{
-            min_height=col_heigths[collumn];
+        if min_height > col_heigth[column]{
+            min_height=col_heigth[column];
         }
-        if max_height > col_heigths[collumn]{
-            max_height=col_heigths[collumn];
+        if max_height > col_heigth[column]{
+            max_height=col_heigth[column];
         }
-        if collumn >=1{
-            if value.abs(col_heigths[collumn-1]-col_heigths[collumn])>max_side_diff{
-                max_side_diff=value.abs(col_heigths[collumn-1]-col_heigths[collumn]);
+        if column >=1{
+            if value.abs(col_heigth[column -1]-col_heigth[column])>max_side_diff{
+                max_side_diff=value.abs(col_heigth[column -1]-col_heigth[column]);
             }
         }
     }
@@ -57,13 +55,13 @@ fn compute_best_place(/*matrix,w1,w2,w3,w4*/){
 
 /*this function will check for each possiblr position of the tetromino which one is the better.
 Then it will call function to move the piece to the right place*/
-fn find_best_place(/* tetromino et sol*/){
-    let score = 0;
-    let best_score = 0;
-    let pose = [0,0];
-    for col in range 0..9{
-        for rotate in range 0..3{
-            score = compute_best_place(col,rotate);
+fn find_best_place(mat : &[[bool; 144]; 160], piece : &piece){
+    let mut score :u8 = 0;
+    let mut best_score = 0;
+    let mut pose = [0,0];
+    for col in 0..9{
+        for rotate in  0..3{
+            score = compute_best_place(mat,col,rotate);
             if score > best_score{
                 best_score = score;
                 pose[0] = col;
