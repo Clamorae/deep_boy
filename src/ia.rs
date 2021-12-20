f5839108509d3db697643790400d5066cc34f1a1
Clamorae <trombiniquentin@gmail.com>
2021-12-20 23:59:16 +0100

    technically it's working :pray:

--- src/ia.rs
+++ src/ia.rs
@@ -5,8 +5,6 @@ pub struct Ia{
     pub mat :[[bool; 10]; 18],
     pub old_mat : [[bool; 10]; 18],
     pub tet : PieceType,
-    pub inputs : [Input; 8], //TODO Check le nombre max de coup ?
-    pub input_iterator : u8
     //child : [u8; 4]
     //Jeux de coup ?
     //Je commente car flemme
@@ -26,8 +24,7 @@ pub enum PieceType{
 pub enum Input{
     Left,
     Right,
-    A,
-    None
+    A
 }
 
 //tile: 47
@@ -36,7 +33,6 @@ pub enum Input{
 //+0x20 pour l'axe y
 impl Ia{
 
-    /*Generate the matrix containing already set tetriminos*/
     pub fn get_field(&mut self, mem: &mut Memory){
         self.old_mat = self.mat;
 
@@ -93,83 +89,17 @@ impl Ia{
     }
 
     pub fn get_inputs(&mut self) -> Controls {
-        let mut temp: Controls;
-        match self.inputs[self.input_iterator] {
-            Left => {
-                self.ready_next_move();
-                temp = Controls {
-                    up: 1,
-                    down: 1,
-                    left: 0,
-                    right: 1,
-                    a: 1,
-                    b: 1,
-                    select: 1,
-                    start: 1,
-                }
-            },
-            Right => {
-                self.ready_next_move();
-                temp = Controls {
-                    up: 1,
-                    down: 1,
-                    left: 1,
-                    right: 0,
-                    a: 1,
-                    b: 1,
-                    select: 1,
-                    start: 1,
-                }
-            },
-            A => {
-                self.ready_next_move();
-                temp = Controls {
-                    up: 1,
-                    down: 1,
-                    left: 1,
-                    right: 1,
-                    a: 0,
-                    b: 1,
-                    select: 1,
-                    start: 1,
-                }
-            },
-            None => {
-                self.ready_next_move();
-                temp = Controls { //TODO METTRE EN DOWN ?
-                    up: 1,
-                    down: 0,
-                    left: 1,
-                    right: 1,
-                    a: 1,
-                    b: 1,
-                    select: 1,
-                    start: 1,
-                }
-            },
-        }
-        return temp;
-    }
-
-    /*Lunched at each new screen, this function will act as the routine for our Ia*/
-    pub fn process_screen(&mut self, mem: &mut Memory) {
-        self.get_field(mem); //Generating the new screen
-        if self.mat =! self.old_mat {
-            //Calculate the new list of inpu for the new piece
-            self.input_iterator = 0 //Reseting the parsing of inputs
+        Controls{
+            up: 1,
+            down: 1,
+            left: 1,
+            right: 1,
+            a: 1,
+            b: 1,
+            select: 1,
+            start: 1,
         }
     }
-
-    pub fn ready_next_move(&mut self) {
-        if self.input_iterator == 7{
-            self.input_iterator = 0
-        }else{
-            self.input_iterator += 1;
-        }
-    }
-
-
-
 
 }
 
@@ -177,7 +107,7 @@ impl Ia{
 
 /*This function will take a position and then compute a score. Lower is the score better is the position.*/
 //TODO Make a enum for rotate ?
-fn compute_best_place(matrix : &[[bool; 144]; 160],col : u8, rotate : u8, ) -> u8{
+fn compute_best_place(matrix : &[[bool; 144]; 160],col : u8) -> u8{
     /*gaps = number of gap
     height_mean = mean of the heigths
     max_diff diff between highest and lowest
@@ -220,22 +150,104 @@ fn find_best_place(mat : &[[bool; 144]; 160], piece : &piece) -> [Input] {
     the first array in each line represent the base piece shape
     each other array in line represent the base piece shape ater a counterclockwise rotation 
     */
-    let tet_patern = [
-                        [[15,0,0,15],[15,0,0,15],[15,0,0,15],[15,0,0,15]],//o
-                        [[1,0,1,15],[15,0,1,15],[0,0,0,15],[15,1,0,15]],//t
-                        [[0,0,1,15],[15,1,0,15],[0,0,1,15],[15,1,0,15]],//s
-                        [[0,0,0,15],[15,2,0,15],[0,1,1,15],[15,0,0,15]],//l
-                        [[0,0,0,15],[15,0,0,15],[1,1,0,15],[15,0,2,15]],//j
-                        [[0,0,0,0],[15,0,15,15],[0,0,0,0],[15,0,15,15]],//i
-                        [[1,0,0,15],[15,0,1,15],[1,0,0,15],[15,0,1,15]]//z
-                        ]
+    let mut tet_patern;
+    match self.tet {
+        O => tet_patern = [[0,0],[0,0],[0,0],[0,0]],
+        T => tet_patern = [[1,0,1],[0,1],[0,0,0],[1,0]],
+        S => tet_patern = [[0,0,1],[1,0],[0,0,1],[1,0]],
+        L => tet_patern = [[0,0,0],[2,0],[0,1,1],[0,0]],
+        J => tet_patern = [[0,0,0],[0,0],[1,1,0],[0,2]],
+        I => tet_patern = [[0,0,0,0],[0],[0,0,0,0],[0]],
+        Z => tet_patern = [[1,0,0],[0,1],[1,0,0],[0,1]]
+    }
     let mut score :u8 = 0;
     let mut best_score = 0;
     let mut pose = [0,0];
+    let mut piece_shape;
+    let mut new_mat;
+    let mut is_placed;
+    let mut heigth
+
     for col in 0..9{
         for rotate in  0..3{
-            //create matrix with the tet
-            score = compute_best_place(mat,col,rotate);
+            piece_shape = tet_patern[rotate];
+            new_mat = mat;
+            is_placed = 0;
+            heigth = 15;
+            if piece_shape.len()+col<9{
+                while is_placed == 0{
+                    for i in range 0..piece_shape.len(){
+                        if new_mat[col+i][heigth+1-piece_shape[i]]==1{
+                            is_placed = 1;
+                            for i in range 0..piece_shape.len(){
+                                new_mat[col+i][heigth-piece_shape[i]] = 1;
+                            }
+                            match self.tet {
+                                O => {
+                                    new_mat[col][heigth-1] = 1;
+                                    new_mat[col+1][heigth-1] = 1;
+                                    },
+                                T => {
+                                    match rotate{
+                                        0 => new_mat[col+1][heigth-1] = 1,
+                                        1 => {new_mat[col][heigth-1] = 1;
+                                              new_mat[col][heigth-2] = 1;},
+                                        2 => new_mat[col+1][heigth-1] = 1,
+                                        3 => {new_mat[col+1][heigth-1] = 1;
+                                            new_mat[col+1][heigth-2] = 1;}
+                                    }
+                                }
+                                S => {
+                                     if rotate==0 || rotate==2{
+                                         new_mat[col][heigth-2] = 1;
+                                         new_mat[col+1][heigth-1] = 1;
+                                     }else{
+                                         new_mat[col+1][heigth-1] = 1;
+                                     }
+                                    },
+                                L => {
+                                        match rotate{
+                                            0 => new_mat[col+2][heigth-1] = 1,
+                                            1 => {new_mat[col+1][heigth-1] = 1;
+                                                  new_mat[col+1][heigth-2] = 1;},
+                                            2 => new_mat[col][heigth-1] = 1,
+                                            3 => {new_mat[col][heigth-1] = 1;
+                                                new_mat[col][heigth-2] = 1;}
+                                        }
+                                    }
+                                J => {
+                                    match rotate{
+                                        0 => new_mat[col][heigth-1] = 1,
+                                        1 => {new_mat[col+1][heigth-1] = 1;
+                                              new_mat[col+1][heigth-2] = 1;},
+                                        2 => new_mat[col+2][heigth-1] = 1,
+                                        3 => {new_mat[col][heigth-1] = 1;
+                                            new_mat[col][heigth-2] = 1;}
+                                    }
+                                }
+                                I => {
+                                    if rotate==1 || rotate==3{
+                                        new_mat[col][heigth-1] = 1;
+                                        new_mat[col][heigth-2] = 1;
+                                        new_mat[col][heigth-3] = 1;
+                                    }
+                                   },
+                                Z => {
+                                    if rotate==0 || rotate== 2{
+                                        new_mat[col][heigth-1] = 1;
+                                        new_mat[col+1][heigth-2] = 1;
+                                    }else{
+                                        new_mat[col+1][heigth-1] = 1;
+                                    }
+                                   }
+                            }
+                        }else{
+                            heigth +=1;
+                        }
+                    }
+                }
+            }
+            score = compute_best_place(new_mat);
             if score > best_score{
                 best_score = score;
                 pose[0] = col;
@@ -243,23 +255,26 @@ fn find_best_place(mat : &[[bool; 144]; 160], piece : &piece) -> [Input] {
             }
         }
     }
+
     let mut future_inputs[pose[0]+pose[1]];
     let mut index = 0;
     pose[0] = pose[0] - 4;
+    for i in range 0..rotate{
+        future_inputs[index] = Input.A;
+            i+=1
+    }
+
     if pose[0] > 0 {
         for i in range 0..pose[0]{
             future_inputs[index] = Input.Right;
             i+=1
         }
+
     }else if pose[0]<0{
         for i in range 0..value.abs(pose[0]){
             future_inputs[index] = Input.Left;
             i+=1
         }
-    }
-    for i in range 0..rotate{
-        future_inputs[index] = Input.A;
-            i+=1
     }
  Return future_inputs;
 }
